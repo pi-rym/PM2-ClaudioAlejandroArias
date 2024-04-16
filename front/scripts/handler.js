a1ea0d3f82d17865229a03ea6f759c349fefc981
claudioarias10 <claudioarias10@gmail.com>
2024-04-16 11:51:55 -0300

    webpack

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -1,7 +1,32 @@
 const renderFilms = require("./renderFilms")
+const axios = require("axios");
 
-const getFilms = () => {
-    $.get("https://students-api.up.railway.app/movies", (data)=> data.map(renderFilms).fail(()=>alert("Error al traer las peliculas")))
+//con Promesas
+// const getFilms = () => {
+//     axios.get("https://students-api.up.railway.app/movies")
+//     .then((response)=>{
+//         // console.log(response);
+//         response.data.map(renderFilms);
+//     }).catch((error) => {
+//         // console.log(error);
+//         alert(error.message);
+//     })
+// }
+
+//Async Await
+const getFilms = async() => {
+    try {
+        const result = await axios.get("https://students-api.up.railway.app/movies")
+        result.data.map(renderFilms);  
+    } catch (error) {
+        console.log(error.message);
+    }
 }
+
+
+
+// const getFilms = () => {
+//     $.get("https://students-api.up.railway.app/movies", (data)=> data.map(renderFilms).fail(()=>alert("Error al traer las peliculas")))
+// }
 
 module.exports = getFilms;
